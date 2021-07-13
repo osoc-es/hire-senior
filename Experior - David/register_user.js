e31f1ae05d3e90776afe6043d5fb41da3ca5f51f
Javi-100 <86244534+Javi-100@users.noreply.github.com>
2021-07-13 17:37:42 +0200

    Update register_user.js

--- Experior - David/register_user.js
+++ Experior - David/register_user.js
@@ -29,25 +29,25 @@ function pasoPagIndex() {
 }
 
 function paso2apaso1() {
-	document.getElementById('paso1').classList.add("d-block");
-	document.getElementById('paso2').classList.remove("d-block");
+	document.getElementById('paso1').className = "container d-block";
+	document.getElementById('paso2').className = "container d-none";
 	document.getElementById('nombreCompleto').innerHTML = document.getElementById('name').value + " " + document.getElementById('1st_last_Name').value;
 }
 
 function paso3apaso2() {
-	document.getElementById('paso2').classList.add("d-block");
-	document.getElementById('paso3').classList.remove("d-block");
+	document.getElementById('paso2').className = "container d-block";
+	document.getElementById('paso3').className = "container d-none";
 	document.getElementById('nombreUsuario1').innerHTML = document.getElementById('username').value;
 	document.getElementById('nombreUsuario2').innerHTML = document.getElementById('username').value;
 	document.getElementById('nombreUsuario3').innerHTML = document.getElementById('username').value;
 }
 
 function paso4apaso3() {
-	document.getElementById('paso3').classList.add("d-block");
-	document.getElementById('paso4').classList.remove("d-block");
+	document.getElementById('paso3').className = "container d-block";
+	document.getElementById('paso4').className = "container d-none";
 }
 
 function paso5apaso4() {
-	document.getElementById('paso4').classList.add("d-block");
-	document.getElementById('paso5').classList.remove("d-block");
+	document.getElementById('paso4').className = "container d-block";
+	document.getElementById('paso5').className = "container d-none";
 }
