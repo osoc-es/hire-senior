a93bda278814190adcc26efd7cea18601acd58e6
Javi-100 <86244534+Javi-100@users.noreply.github.com>
2021-07-14 20:12:32 +0200

    Update register_user.js

--- Experior - David/register_user.js
+++ Experior - David/register_user.js
@@ -1,53 +1,45 @@
 function paso1apaso2() {
-	document.getElementById('paso1').classList.add("d-none");
-	document.getElementById('paso2').classList.remove("d-none");
+
+    document.getElementById("primero").className="row d-none";
+    document.getElementById("segundo").className="row d-block";
+
+
+
+
 	document.getElementById('nombreCompleto').innerHTML = document.getElementById('name').value + " " + document.getElementById('1st_last_Name').value;
 }
 
 function paso2apaso3() {
-	document.getElementById('paso2').classList.add("d-none");
-	document.getElementById('paso3').classList.remove("d-none");
+
+    document.getElementById("segundo").className="row d-none";
+    document.getElementById("tercero").className="row d-block";
+
+
 	document.getElementById('nombreUsuario1').innerHTML = document.getElementById('username').value;
 	document.getElementById('nombreUsuario2').innerHTML = document.getElementById('username').value;
 	document.getElementById('nombreUsuario3').innerHTML = document.getElementById('username').value;
 }
 
-function paso3apaso4() {
-	document.getElementById('paso3').classList.add("d-none");
-	document.getElementById('paso4').classList.remove("d-none");
-}
-
-function paso4apaso5() {
-	document.getElementById('paso4').classList.add("d-none");
-	document.getElementById('paso5').classList.remove("d-none");
-}
 
 /////////////////////////////Retroceder///////////////////////////////
 
-function pasoPagIndex() {
-//poner url a la pagina index
-}
+
 
 function paso2apaso1() {
-	document.getElementById('paso1').className = "container d-block";
-	document.getElementById('paso2').className = "container d-none";
-	document.getElementById('nombreCompleto').innerHTML = document.getElementById('name').value + " " + document.getElementById('1st_last_Name').value;
-}
+    document.getElementById("primero").className="row d-block";
+    document.getElementById("segundo").className="row d-none";
+
+
+
+	}
 
 function paso3apaso2() {
-	document.getElementById('paso2').className = "container d-block";
-	document.getElementById('paso3').className = "container d-none";
+
+    document.getElementById("segundo").className="row d-block";
+    document.getElementById("tercero").className="row d-none";
+
+
 	document.getElementById('nombreUsuario1').innerHTML = document.getElementById('username').value;
 	document.getElementById('nombreUsuario2').innerHTML = document.getElementById('username').value;
 	document.getElementById('nombreUsuario3').innerHTML = document.getElementById('username').value;
-}
-
-function paso4apaso3() {
-	document.getElementById('paso3').className = "container d-block";
-	document.getElementById('paso4').className = "container d-none";
-}
-
-function paso5apaso4() {
-	document.getElementById('paso4').className = "container d-block";
-	document.getElementById('paso5').className = "container d-none";
 }
